fc8f8ad8d5e1d0927a8b9fa6fefc60cd9d8cd4f9
maximerischard <rischardmaxime@gmail.com>
2018-04-26 16:56:08 +0100

    fix bug when support does not overlap

--- src/rand.jl
+++ src/rand.jl
@@ -1,34 +1,44 @@
-function rand!(coup::MaximalCoupling, xy::AbstractVector)
+function rand!(coup::MaximalCoupling{D1,D2}, xy::AbstractVector) where {D1, D2}
     length(xy) == length(coup) ||
         throw(DimensionMismatch("Output size inconsistent with sample length."))
 
     X = rand(coup.p)
     log_pX = logpdf(coup.p, X)
     log_qX = logpdf(coup.q, X)
-    logU = log_pX - randexp()
+    if isnan(log_qX)
+        # logpdf can return NaN if X is outside of
+        # the support of q, in which case we should 
+        # change it to -Inf
+        log_qX = -Inf
+    end
+    logU = -randexp()
     local Y
-    if logU - log_qX <= eps()
+    if logU <= ((log_qX - log_pX) + eps()) # eps gives a bit of margin
         Y = X
     else
         found_uncoupled = false
         for _ in 1:1_000_000 # give up after a while
             Ystar = rand(coup.q)
             log_pYstar = logpdf(coup.p, Ystar)
+            if isnan(log_pYstar)
+                # logpdf can return NaN if Ystar is outside of
+                # the support of p, in which case we should 
+                # change it to -Inf
+                log_pYstar = -Inf
+            end
             log_qYstar = logpdf(coup.q, Ystar)
             if log_qYstar < log_pYstar
                 continue
             end
-            logUstar = log_qYstar - randexp()
-            if logUstar - log_pYstar >= -eps()
+            logUstar = -randexp()
+            if logUstar >= ((log_pYstar - log_qYstar) - eps())
                 Y = Ystar
                 found_uncoupled = true
                 break
             end
         end
         if !found_uncoupled
-            println("failed to find uncoupled sample between ", coup.p, "\n and \n", coup.q)
-        end
-        if !found_uncoupled
+            warn("failed to find uncoupled sample between ", coup.p, "\n and \n", coup.q)
             # just return a coupled sample after all
             Y = X
         end
